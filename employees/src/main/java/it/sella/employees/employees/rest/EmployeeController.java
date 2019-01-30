58bbccf4fc6542f6756d44bacfa077366e4606dd
Tamilmani <tamilmani.muthusamy@gmail.com>
2019-01-30 20:24:45 +0530

    zipkin

--- employees/src/main/java/it/sella/employees/employees/rest/EmployeeController.java
+++ employees/src/main/java/it/sella/employees/employees/rest/EmployeeController.java
@@ -2,6 +2,8 @@ package it.sella.employees.employees.rest;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import it.sella.employees.employees.model.Employee;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.actuate.autoconfigure.endpoint.web.WebEndpointProperties;
 import org.springframework.boot.autoconfigure.hateoas.HateoasProperties;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,21 +20,27 @@ import java.util.Random;
 @RestController
 public class EmployeeController {
 
+    private static final Logger logger = LoggerFactory.getLogger(EmployeeController.class);
+
     @HystrixCommand(fallbackMethod = "getEmployeeFallBack", commandKey = "FBC1", groupKey = "FBG1")
     @GetMapping(path = "/employees")
     public Employee getEmpoyee() {
+        logger.info("Entered into getEmpoyee!...");
         if (new Random().nextBoolean()) {
             throw new RuntimeException("TEST HYSTRIX!..");
         }
+        logger.info("Leaving into getEmpoyee!...");
         return new Employee(4229L,"Tamilmani", "BharathKumar");
     }
 
     @HystrixCommand(fallbackMethod = "getEmployeeFallBack", commandKey = "FBC2", groupKey = "FBG2")
     @GetMapping(path = "/employees1")
     public Employee getEmpoyee1() {
+        logger.info("Entered into getEmpoyee1!...");
         if (new Random().nextBoolean()) {
             throw new RuntimeException("TEST HYSTRIX!..");
         }
+        logger.info("Leaving into getEmpoyee1!...");
         return new Employee(4229L,"Tamilmani", "BharathKumar");
     }
 
