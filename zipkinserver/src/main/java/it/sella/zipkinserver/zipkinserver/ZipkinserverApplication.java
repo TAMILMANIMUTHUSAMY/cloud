beadc0f824d1e166046f6efc2c3aa99a5c254fd3
Tamilmani <tamilmani.muthusamy@gmail.com>
2019-02-01 18:44:24 +0530

    zipkin-sleth

--- zipkinserver/src/main/java/it/sella/zipkinserver/zipkinserver/ZipkinserverApplication.java
+++ zipkinserver/src/main/java/it/sella/zipkinserver/zipkinserver/ZipkinserverApplication.java
@@ -2,8 +2,10 @@ package it.sella.zipkinserver.zipkinserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import zipkin.server.EnableZipkinServer;
 
+@EnableDiscoveryClient
 @EnableZipkinServer
 @SpringBootApplication
 public class ZipkinserverApplication {
